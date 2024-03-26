526103d72be0daec87fd5fd731c14641c34c24a8
meozalat2022 <meozalat2002@hotmail.com>
2024-03-26 23:07:36 +0200

    adding mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const app = express();
 
 const port = process.env.PORT || 8080;
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then((result) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.log(err));
 
 app.listen(port, () => console.log(`Server is running on port${port}`));
