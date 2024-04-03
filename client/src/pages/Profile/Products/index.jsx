9e00e03007a3d7c6c28d460ddf7ba6e64a026085
meozalat2022 <meozalat2002@hotmail.com>
2024-04-03 02:11:28 +0200

    get products

--- client/src/pages/Profile/Products/index.jsx
+++ client/src/pages/Profile/Products/index.jsx
@@ -1,9 +1,40 @@
-import { Button } from "antd";
-import React, { useState } from "react";
+import { Button, Table, message } from "antd";
+import React, { useState, useEffect } from "react";
 import ProductsForm from "./ProductsForm";
-
+import { useDispatch, useSelector } from "react-redux";
+import { setLoader } from "../../../redux/loaderSlice";
+import { GetProducts } from "../../../apicalls/products";
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
   const [showProductForm, setShowProductForm] = useState(false);
+  const columns = [
+    { title: "Name", dataIndex: "name" },
+    { title: "Description", dataIndex: "description" },
+    { title: "Price", dataIndex: "price" },
+    { title: "Category", dataIndex: "category" },
+    { title: "Age", dataIndex: "age" },
+    { title: "Status", dataIndex: "status" },
+    { title: "Action", dataIndex: "action" },
+  ];
+  console.log(products);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        dispatch(setLoader(true));
+        const response = await GetProducts();
+        dispatch(setLoader(false));
+
+        if (response.success) {
+          setProducts(response.products);
+        }
+      } catch (error) {
+        dispatch(setLoader(false));
+        message.error(error.message);
+      }
+    };
+    getData();
+  }, []);
   return (
     <div>
       <div className="flex justify-end">
@@ -11,6 +42,7 @@ const Products = () => {
           Add Product
         </Button>
       </div>
+      <Table columns={columns} dataSource={products} />
       {showProductForm && (
         <ProductsForm
           showProductForm={showProductForm}
