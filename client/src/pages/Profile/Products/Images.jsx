671b2cfa220c5bc86b298aaf00fcb9c25b00bb1f
meozalat2022 <meozalat2002@hotmail.com>
2024-04-07 15:10:41 +0200

    delete product image

--- client/src/pages/Profile/Products/Images.jsx
+++ client/src/pages/Profile/Products/Images.jsx
@@ -3,7 +3,7 @@ import { Button, message } from "antd";
 import Upload from "antd/es/upload/Upload";
 import { useDispatch } from "react-redux";
 import { setLoader } from "../../../redux/loaderSlice";
-import { UploadImages } from "../../../apicalls/products";
+import { EditProduct, UploadImages } from "../../../apicalls/products";
 const Images = ({
   showProductForm,
   setShowProductForm,
@@ -37,8 +37,38 @@ const Images = ({
       message.error(error.message);
     }
   };
+
+  const deleteImage = async (image) => {
+    try {
+      dispatch(setLoader(true));
+      const updatedImageArray = images.filter((item) => item !== image);
+      const updatedProduct = { ...selectedProduct, images: updatedImageArray };
+      const response = await EditProduct(selectedProduct._id, updatedProduct);
+      if (response.success) {
+        message.success(response.message);
+        setImages(updatedImageArray);
+        getData();
+      } else {
+        throw new Error(response.message);
+      }
+    } catch (error) {
+      dispatch(setLoader(false));
+      message.error(error.message);
+    }
+  };
   return (
     <div>
+      <div className="flex gap-5 mb-5">
+        {images.map((image) => (
+          <div className="flex gap-2 border border-solid border-slate-500 rounded p-2 items-end">
+            <img className="h-20 w-20 object-cover" src={image} alt="" />
+            <i
+              onClick={() => deleteImage(image)}
+              className="ri-delete-bin-line"
+            ></i>
+          </div>
+        ))}
+      </div>
       <Upload
         listType="picture"
         onChange={(info) => {
@@ -48,17 +78,6 @@ const Images = ({
         beforeUpload={() => false}
         showUploadList={showPreview}
       >
-        <div className="flex gap-5 mb-5">
-          {images.map((image) => (
-            <div className="flex gap-2 border border-solid border-slate-500 rounded p-2 items-end">
-              <img className="h-20 w-20 object-cover" src={image} alt="" />
-              <i
-                // onClick={() => ()}
-                className="ri-delete-bin-line"
-              ></i>
-            </div>
-          ))}
-        </div>
         <Button type="dashed">Upload Images</Button>
       </Upload>
       <div className="flex justify-end gap-5">
