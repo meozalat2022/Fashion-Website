a0d2daf50c85ffbbdcc6f0c87d4057d7811ffb37
meozalat2022 <meozalat2002@hotmail.com>
2024-04-09 00:59:59 +0200

    adding product details

--- client/src/pages/ProductInfo/index.jsx
+++ client/src/pages/ProductInfo/index.jsx
@@ -5,6 +5,7 @@ import { setLoader } from "../../redux/loaderSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import Divider from "../../components/Divider";
+import moment from "moment";
 const ProductInfo = () => {
   const [product, setProduct] = useState(null);
   const [selectedImage, setSelectedImage] = useState(0);
@@ -32,8 +33,8 @@ const ProductInfo = () => {
   }, []);
   return (
     product && (
-      <div>
-        <div className="grid grid-cols-2">
+      <div className="mt-5">
+        <div className="grid grid-cols-2 gap-8">
           {/* images */}
 
           <div className="flex flex-col gap-5">
@@ -56,6 +57,69 @@ const ProductInfo = () => {
                   onClick={() => setSelectedImage(index)}
                 />
               ))}
+            </div>
+            <Divider />
+            <div>
+              <h1 className="text-slate-600">Added On</h1>
+              <span className="text-slate-600">
+                {moment(product.createdAt).format("MMM DD, YYYY hh:mm A")}
+              </span>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <div>
+              <h1 className="text-2xl font-semibold text-orange-900">
+                {product?.name}
+              </h1>
+              <span className="">{product?.description}</span>
+            </div>
+            <Divider />
+            <div className="flex flex-col">
+              <h1 className="text-2xl font-semibold text-orange-900">
+                Product Details
+              </h1>
+              <div className="flex justify-between mt-2">
+                <span>Price</span>
+                <span>$ {product?.price}</span>
+              </div>
+              <div className="flex justify-between mt-2">
+                <span>Category</span>
+                <span>{product?.category}</span>
+              </div>
+              <div className="flex justify-between mt-2">
+                <span>Bill Available</span>
+                <span>{product?.billAvailable ? "Yes" : "No"}</span>
+              </div>
+              <div className="flex justify-between mt-2">
+                <span>Box Available</span>
+                <span>{product?.boxAvailable ? "Yes" : "No"}</span>
+              </div>
+              <div className="flex justify-between mt-2">
+                <span>Accessories Available</span>
+                <span>{product?.accessoriesAvailable ? "Yes" : "No"}</span>
+              </div>
+              <div className="flex justify-between mt-2">
+                <span>Warranty Available</span>
+                <span>{product?.warrantyAvailable ? "Yes" : "No"}</span>
+              </div>
+            </div>
+            <Divider />
+
+            {/* seller details */}
+
+            <div className="flex flex-col">
+              <h1 className="text-2xl font-semibold text-orange-900">
+                Seller Details
+              </h1>
+              <div className="flex justify-between mt-2">
+                <span>Name</span>
+                <span>{product?.seller?.name}</span>
+              </div>
+
+              <div className="flex justify-between mt-2">
+                <span>ُEmail</span>
+                <span>{product?.seller?.email}</span>
+              </div>
             </div>
           </div>
         </div>
